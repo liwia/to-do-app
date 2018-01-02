a750b3fc05d59bd851194d8d63c8cb04e99313de
liwia <liwia.topolska@gmail.com>
2018-01-02 15:42:28 +0100

    adding state

--- src/TaskMainView.js
+++ src/TaskMainView.js
@@ -1,6 +1,11 @@
 import React, { Component } from 'react';
+import TaskList from './TaskList';
+import TaskForm from './TaskForm';
 
 class TaskMainView extends Component {
+    state = {
+        tasks: []
+    };
     render() {
         return (
             <div>
