c8f5f2a519e9a5a60532c25ac8fe1f989dd85a05
liwia <liwia.topolska@gmail.com>
2018-01-02 16:02:25 +0100

    handling change of input value

--- src/TaskForm.js
+++ src/TaskForm.js
@@ -4,11 +4,17 @@ class TaskForm extends Component {
     state = {
         taskInputValue: ''
     };
+    handleChange = (event) => {
+        this.setState({
+            taskInputValue: event.target.value
+        })
+    };
     render() {
         return (
             <form>
                 <input
                     value={this.state.taskInputValue}
+                    onChange={this.handleChange}
                 />
                 <button>Add task</button>
             </form>
