3bfba062b5747a6a762437b8e625e2072d13115b
liwia <liwia.topolska@gmail.com>
2018-01-02 16:14:21 +0100

    adding addTask method

--- src/TaskMainView.js
+++ src/TaskMainView.js
@@ -6,11 +6,29 @@ class TaskMainView extends Component {
     state = {
         tasks: []
     };
+    addTask = (taskContent) => {
+        if(taskContent.trim() === ''){
+            return;
+        }
+        this.setState({
+            tasks: this.state.tasks.concat({
+                id: this.state.tasks.map(
+                    task => task.id
+                ).reduce(
+                    (biggest, next) => Math.max(biggest, next),
+                    0
+                ) + 1,
+                content: taskContent
+            })
+        });
+    };
     render() {
         return (
             <div>
-                <TaskForm />
-                <TaskList/>
+                <TaskForm
+                    addTask={this.addTask}/>
+                <TaskList
+                    tasks={this.state.tasks}/>
             </div>
 
         );
