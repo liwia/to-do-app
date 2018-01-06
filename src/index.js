a61e6bc77fda86f97e18c009e414ddfa9342ae9a
liwia <liwia.topolska@gmail.com>
2018-01-06 18:19:29 +0100

    wip

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import setupFirebase from './setupFirebase';
 import App from './App';
+import ToDoFirebase from './ToDoFirebase'
 import registerServiceWorker from './registerServiceWorker';
 
 setupFirebase();
