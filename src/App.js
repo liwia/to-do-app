99dfa236872a250a21b23b0a07b9dee673f90326
liwia <liwia.topolska@gmail.com>
2018-01-07 20:20:21 +0100

    auth

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import './App.css';
 import Auth from './Auth';
 import TaskMainView from './TaskMainView';
@@ -6,7 +6,7 @@ import SearchBar from './SearchBar';
 import ToDoFirebase from './ToDoFirebase';
 
 class App extends Component {
-    state={
+    state = {
         searchPhrase: ''
     }
 
@@ -15,26 +15,27 @@ class App extends Component {
             searchPhrase: text
         })
     }
-  render() {
-    return (
-        <Auth>
-      <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Welcome to To Do Task List</h1>
-        </header>
-        <p className="App-intro">
-          To get started, add your tasks!
-        </p>
-          <SearchBar
-              onSearch={this.onSearch}
-          />
-        <TaskMainView
-            searchPhrase={this.state.searchPhrase}/>
-        <ToDoFirebase/>
-      </div>
-        </Auth>
-    );
-  }
+
+    render() {
+        return (
+            <Auth>
+                <div className="App">
+                    <header className="App-header">
+                        <h1 className="App-title">Welcome to To Do Task List</h1>
+                    </header>
+                    <p className="App-intro">
+                        To get started, add your tasks!
+                    </p>
+                    <SearchBar
+                        onSearch={this.onSearch}
+                    />
+                    <TaskMainView
+                        searchPhrase={this.state.searchPhrase}/>
+                    <ToDoFirebase/>
+                </div>
+            </Auth>
+        );
+    }
 }
 
 export default App;
